dfd74987726985ef107d71f76de76959f5f1cdb7
Anastasiya <dpacha@ya.ru>
2025-05-26 14:23:56 +0300

    fix http.module in authUserApp

--- auth-user-app/src/http/http.module.ts
+++ auth-user-app/src/http/http.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthcheckModule } from '~src/http/modules/healthcheck/healthcheck.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { UserInfoTestModule } from '~src/http/modules/user-info-test/user-info-test.module';
 
 @Module({
-    imports: [HealthcheckModule, AuthModule, UserInfoTestModule],
+    imports: [HealthcheckModule, AuthModule],
 })
 export class HttpModule {}
